3a9dac85597c5d001920a21727142bbc879243d1
Oleg Egorov <st76@yandex.ru>
2023-01-22 23:20:02 +0400

    Внес правки в регулярное выражение

--- postSize.js
+++ postSize.js
@@ -1,5 +1,5 @@
 function postSize(message) {
-  const str = message.replace(/[\a-z0-9/\/./:]/gi, '');
+  const str = message.replace(/[a-z0-9:/._\+~#=@]{2,}\.[a-z]{2,6}/gi, '');
   const len = str.length;
   return len;
 }
